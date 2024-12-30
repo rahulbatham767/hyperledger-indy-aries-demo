dc0ad3ea72e31f9fd71722b85263004d9f622a3c
rahulbatham767 <rahulbatham767@gmail.com>
2024-12-31 00:18:55 +0530

    http header set for https

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,20 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply headers to all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src * 'self' http: https: 'unsafe-inline' 'unsafe-eval';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
